755188da992ab7496519cf1804187598f572ab75
Csaba Hermann <hcsabi98@gmail.com>
2019-05-18 22:13:21 +0200

    Corrected the fast power

--- src/main/java/RSA/tools/FastPower.java
+++ src/main/java/RSA/tools/FastPower.java
@@ -5,11 +5,14 @@ import java.math.BigInteger;
 public class FastPower {
     public static BigInteger calculate(BigInteger base, BigInteger exponent, BigInteger modulo) {
         String numberInBinary = exponent.toString(2);
-        BigInteger multiplication = (base.pow(1)).mod(modulo);
-        BigInteger previous = multiplication;
+        BigInteger multiplication = BigInteger.ONE;
+        BigInteger previous = (base.pow(1)).mod(modulo);
+        if (numberInBinary.charAt(numberInBinary.length() - 1) == '1') {
+            multiplication = previous;
+        }
         for (int i = 1; i < numberInBinary.length(); i++) {
             previous = (previous.pow(2)).mod(modulo);
-            if (numberInBinary.charAt(numberInBinary.length() - i) == 1) {
+            if (numberInBinary.charAt(numberInBinary.length() - (i + 1)) == '1') {
                 multiplication = multiplication.multiply(previous);
             }
         }
