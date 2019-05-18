8ea967456c21f1e6966ca21035f6493655228657
Csaba Hermann <hcsabi98@gmail.com>
2019-05-18 14:11:08 +0200

    Fast Power

---
+++ src/main/java/RSA/tools/FastPower.java
@@ -0,0 +1,18 @@
+package RSA.tools;
+
+import java.math.BigInteger;
+
+public class FastPower {
+    public static BigInteger calculate(BigInteger base, BigInteger exponent, BigInteger modulo) {
+        String numberInBinary = exponent.toString(2);
+        BigInteger multiplication = (base.pow(1)).mod(modulo);
+        BigInteger previous = multiplication;
+        for (int i = 1; i < numberInBinary.length(); i++) {
+            previous = (previous.pow(2)).mod(modulo);
+            if (numberInBinary.charAt(numberInBinary.length() - i) == 1) {
+                multiplication = multiplication.multiply(previous);
+            }
+        }
+        return multiplication.mod(modulo);
+    }
+}
